2f5464c2402acd57ed0850804034c539ddfd7199
5paun <web5paun@gmail.com>
2019-03-08 01:26:23 +0300

    test check

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,8 @@
 let add = (a,b) => a+b;
 console.log(add(2,6));
-
+let title = document.getElementById('title');
+title.style.color = 'red';
+/*
 $(function () {
     $('.content').css('color', 'red');
-  }); 
+  }); */
