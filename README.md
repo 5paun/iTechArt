f749cb5f49d621e08faefbcb9941842e422805c4
5paun <web5paun@gmail.com>
2019-03-20 01:25:54 +0300

    add table products

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # :iTechArt JS FULL-STACK LAB
-# Project name - Simple eShop
+## Project name - Simple eShop
 
 To open the website, you need to switch to the branch "withoutWebpack" --> to clone remote repositories on your PC --> to run the file index.html in your favorite browsers
--- scripts/admin.js
+++ scripts/admin.js
@@ -85,6 +85,118 @@ const users = [
     }
 ]
 
+const products = [
+    {
+        "id": 1,
+        "title": "Product1",
+        "description": "It's very good thing!-1",
+        "price": "450$",
+        "picture": "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCyeb-r_lrGwSN7UESC-DIz-JWXNbsOMkAW7IH8HSToF1WuzobuA' alt='Ноутбук'>",
+        "tags": "tag-1, tag-2, tag-3, tag-4, tag-5",
+        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='12'>Large button</button>"
+    },
+    {
+        "id": 2,
+        "title": "Product2",
+        "description": "It's very good thing!-2",
+        "price": "450$",
+        "picture": "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCyeb-r_lrGwSN7UESC-DIz-JWXNbsOMkAW7IH8HSToF1WuzobuA' alt='Ноутбук'>",
+        "tags": "tag-1, tag-2, tag-3, tag-4, tag-5",
+        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='12'>Large button</button>"
+    },
+    {
+        "id": 3,
+        "title": "Product3",
+        "description": "It's very good thing!-3",
+        "price": "450$",
+        "picture": "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCyeb-r_lrGwSN7UESC-DIz-JWXNbsOMkAW7IH8HSToF1WuzobuA' alt='Ноутбук'>",
+        "tags": "tag-1, tag-2, tag-3, tag-4, tag-5",
+        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='12'>Large button</button>"
+    },
+    {
+        "id": 4,
+        "title": "Product4",
+        "description": "It's very good thing!-4",
+        "price": "450$",
+        "picture": "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCyeb-r_lrGwSN7UESC-DIz-JWXNbsOMkAW7IH8HSToF1WuzobuA' alt='Ноутбук'>",
+        "tags": "tag-1, tag-2, tag-3, tag-4, tag-5",
+        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='12'>Large button</button>"
+    },
+    {
+        "id": 5,
+        "title": "Product5",
+        "description": "It's very good thing!-5",
+        "price": "450$",
+        "picture": "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCyeb-r_lrGwSN7UESC-DIz-JWXNbsOMkAW7IH8HSToF1WuzobuA' alt='Ноутбук'>",
+        "tags": "tag-1, tag-2, tag-3, tag-4, tag-5",
+        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='12'>Large button</button>"
+    },
+    {
+        "id": 6,
+        "title": "Product6",
+        "description": "It's very good thing!-6",
+        "price": "450$",
+        "picture": "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCyeb-r_lrGwSN7UESC-DIz-JWXNbsOMkAW7IH8HSToF1WuzobuA' alt='Ноутбук'>",
+        "tags": "tag-1, tag-2, tag-3, tag-4, tag-5",
+        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='12'>Large button</button>"
+    },
+    {
+        "id": 7,
+        "title": "Product7",
+        "description": "It's very good thing!-7",
+        "price": "450$",
+        "picture": "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCyeb-r_lrGwSN7UESC-DIz-JWXNbsOMkAW7IH8HSToF1WuzobuA' alt='Ноутбук'>",
+        "tags": "tag-1, tag-2, tag-3, tag-4, tag-5",
+        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='12'>Large button</button>"
+    },
+    {
+        "id": 8,
+        "title": "Product8",
+        "description": "It's very good thing!-8",
+        "price": "450$",
+        "picture": "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCyeb-r_lrGwSN7UESC-DIz-JWXNbsOMkAW7IH8HSToF1WuzobuA' alt='Ноутбук'>",
+        "tags": "tag-1, tag-2, tag-3, tag-4, tag-5",
+        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='12'>Large button</button>"
+    },
+    {
+        "id": 9,
+        "title": "Product9",
+        "description": "It's very good thing!-9",
+        "price": "450$",
+        "picture": "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCyeb-r_lrGwSN7UESC-DIz-JWXNbsOMkAW7IH8HSToF1WuzobuA' alt='Ноутбук'>",
+        "tags": "tag-1, tag-2, tag-3, tag-4, tag-5",
+        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='12'>Large button</button>"
+    },
+    {
+        "id": 10,
+        "title": "Product10",
+        "description": "It's very good thing!-10",
+        "price": "450$",
+        "picture": "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCyeb-r_lrGwSN7UESC-DIz-JWXNbsOMkAW7IH8HSToF1WuzobuA' alt='Ноутбук'>",
+        "tags": "tag-1, tag-2, tag-3, tag-4, tag-5",
+        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='12'>Large button</button>"
+    },
+    {
+        "id": 11,
+        "title": "Product11",
+        "description": "It's very good thing!-11",
+        "price": "450$",
+        "picture": "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCyeb-r_lrGwSN7UESC-DIz-JWXNbsOMkAW7IH8HSToF1WuzobuA' alt='Ноутбук'>",
+        "tags": "tag-1, tag-2, tag-3, tag-4, tag-5",
+        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='12'>Large button</button>"
+    },
+    {
+        "id": 12,
+        "title": "Product12",
+        "description": "It's very good thing!-12",
+        "price": "450$",
+        "picture": "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCyeb-r_lrGwSN7UESC-DIz-JWXNbsOMkAW7IH8HSToF1WuzobuA' alt='Ноутбук'>",
+        "tags": "tag-1, tag-2, tag-3, tag-4, tag-5",
+        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='12'>Large button</button>"
+    }
+]
+
+/* ====== Добавление пользователей в таблицу ============*/
 localStorage.setItem("users", JSON.stringify(users));
 function addUsers(table) { 
   const tableUsers = document.getElementById(table);
@@ -103,11 +215,38 @@ function addUsers(table) {
 }
 addUsers("addUser");
 
-
-
+/* ====== Удаление пользователей из таблицы ============*/
 $("#addUser").on("click", "button[type='button']", function() {
     //let btnData = $(this).attr("data-id");     
     $(this).parent().parent().remove();
     localStorage.removeItem("users");   
-    });
-    
+});
+
+/* ====== Добавление товара в таблицу ============*/
+localStorage.setItem("products", JSON.stringify(products));
+function addProducts(table) { 
+  const tableProducts = document.getElementById(table);
+  let addRow;
+  let products = JSON.parse(localStorage.getItem("products"));
+  if (tableProducts) {
+    for (let i = 0; i < products.length; i++) {        
+        addRow = tableProducts.insertRow();
+        addRow.innerHTML = "<th>" + products[i].id + "</th>" +
+        "<td>" + products[i].title + "</td>" +
+        "<td>" + products[i].description + "</td>" +
+        "<td>" + products[i].price + "</td>" +        
+        "<td>" + products[i].picture + "</td>" +
+        "<td>" + products[i].tags + "</td>" +
+        "<td>" + products[i].remove + "</td>";
+    }
+  }
+}
+addProducts("addProduct");
+
+/* ====== Удаление товара из таблицы ============*/
+$("#addProduct").on("click", "button[type='button']", function() {
+    //let btnData = $(this).attr("data-id");     
+    $(this).parent().parent().remove();
+    localStorage.removeItem("products");   
+});
+
