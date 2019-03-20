ea8a5ddbe5e9132793663eb51b9109695e529b4a
5paun <web5paun@gmail.com>
2019-03-21 01:03:55 +0300

    created form add products

--- scripts/admin.js
+++ scripts/admin.js
@@ -4,84 +4,96 @@ const users = [
         "firstName": "Name1",
         "lastName": "Surname1",
         "email": "mail1@mail.ru",
-        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='1'>Remove</button>"
+        "removeRequest": "",
+        "removeUser": "<button type='button' class='btn btn-secondary btn-lg' data-id='1'>Remove</button>"
     },
     {
         "id": 2,
         "firstName": "Name2",
         "lastName": "Surname2",
         "email": "mail2@mail.ru",
-        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='2'>Remove</button>"
+        "removeRequest": "",
+        "removeUser": "<button type='button' class='btn btn-secondary btn-lg' data-id='2'>Remove</button>"
     },
     {
         "id": 3,
         "firstName": "Name3",
         "lastName": "Surname3",
         "email": "mail3@mail.ru",
-        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='3'>Remove</button>"
+        "removeRequest": "",
+        "removeUser": "<button type='button' class='btn btn-secondary btn-lg' data-id='3'>Remove</button>"
     },
     {
         "id": 4,
         "firstName": "Name4",
         "lastName": "Surname4",
         "email": "mail4@mail.ru",
-        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='4'>Remove</button>"
+        "removeRequest": "",
+        "removeUser": "<button type='button' class='btn btn-secondary btn-lg' data-id='4'>Remove</button>"
     },
     {
         "id": 5,
         "firstName": "Name5",
         "lastName": "Surname5",
         "email": "mail5@mail.ru",
-        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='5'>Remove</button>"
+        "removeRequest": "",
+        "removeUser": "<button type='button' class='btn btn-secondary btn-lg' data-id='5'>Remove</button>"
     },
     {
         "id": 6,
         "firstName": "Name6",
         "lastName": "Surname6",
         "email": "mail6@mail.ru",
-        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='6'>Remove</button>"
+        "removeRequest": "",
+        "removeUser": "<button type='button' class='btn btn-secondary btn-lg' data-id='6'>Remove</button>"
     },
     {
         "id": 7,
         "firstName": "Name7",
         "lastName": "Surname7",
         "email": "mail7@mail.ru",
-        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='7'>Remove</button>"
+        "removeRequest": "",
+        "removeUser": "<button type='button' class='btn btn-secondary btn-lg' data-id='7'>Remove</button>"
     },
     {
         "id": 8,
         "firstName": "Name8",
         "lastName": "Surname8",
         "email": "mail8@mail.ru",
-        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='8'>Remove</button>"
+        "removeRequest": "",
+        "removeUser": "<button type='button' class='btn btn-secondary btn-lg' data-id='8'>Remove</button>"
     },
     {
         "id": 9,
         "firstName": "Name8",
         "lastName": "Surname8",
         "email": "mail8@mail.ru",
-        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='9'>Remove</button>"
+        "removeRequest": "",
+        "removeUser": "<button type='button' class='btn btn-secondary btn-lg' data-id='9'>Remove</button>"
     },
     {
         "id": 10,
         "firstName": "Name9",
         "lastName": "Surname9",
         "email": "mail9@mail.ru",
-        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='10'>Remove</button>"
+        "removeRequest": "",
+        "removeUser": "<button type='button' class='btn btn-secondary btn-lg' data-id='10'>Remove</button>"
     },
     {
         "id": 11,
         "firstName": "Name10",
         "lastName": "Surname10",
         "email": "mail10@mail.ru",
-        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='11'>Remove</button>"
+        "removeRequest": "",
+        "removeUser": "<button type='button' class='btn btn-secondary btn-lg' data-id='11'>Remove</button>"
     },
     {
         "id": 12,
         "firstName": "Name11",
         "lastName": "Surname11",
         "email": "mail11@mail.ru",
-        "remove": "<button type='button' class='btn btn-secondary btn-lg' data-id='12'>Remove</button>"
+        "removeRequest": "",
+        "removeUser": "<button type='button' class='btn btn-secondary btn-lg' data-id='12'>Remove</button>"
     }
 ]
 
@@ -209,7 +221,8 @@ function addUsers(table) {
         "<td>" + users[i].firstName + "</td>" +
         "<td>" + users[i].lastName + "</td>" +
         "<td>" + users[i].email + "</td>" +        
-        "<td>" + users[i].remove + "</td>";
+        "<td>" + users[i].removeRequest + "</td>" +
+        "<td>" + users[i].removeUser + "</td>";
     }
   }
 }
