5745cc31ad7e2cf57be3ed8268948f3f8db68eb3
5paun <web5paun@gmail.com>
2019-04-05 22:03:49 +0300

    Fixed linter errors

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: 'airbnb',
+  globals: {
+    $: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'linebreak-style': 'off',
+    'func-names': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+  },
+};
